a2f643d81aba123c9b1182ec302fcb293df80442
Ramon Moorlag <r.moorlag@gmail.com>
2020-03-01 07:38:08 +0100

    Create turtle_demo_circles.py

---
+++ turtle/turtle_demo_circles.py
@@ -0,0 +1,18 @@
+from turtle import *
+import turtle
+import random
+import math #needed for using square root for example
+import turtle #needed for drawing purposes
+
+goto (20,20)
+
+x = 320
+y = 220
+r = 35
+voor = 14
+
+for i in range(50):
+  turtle.pencolor("red")
+  circle(r)
+  forward (voor)
+  right (r)
